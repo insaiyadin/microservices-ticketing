6b074737e49b7d8cb480959d746c1cd5f526cd5c
Adrian Kierenkiewicz <adrian@kierenkiewicz.pl>
2023-06-05 00:03:47 +0200

    auth update

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,15 +7,15 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    console.log(err.errors);
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    console.log(err.reason);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
-    message: err.message,
+    message: [
+      {
+        message: "Something went wrong",
+      },
+    ],
   });
 };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -12,9 +14,9 @@ router.post(
     body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Passwored must be between 4 and 20 characters"),
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,10 +25,19 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user ..r.");
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError("This user already exists");
+    }
+
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
